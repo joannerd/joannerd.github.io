9c7c32c7baa91bc0c4bc8076a44dd759616eff67
Joanna Chen <x.chenj@gmail.com>
2020-02-25 03:47:53 -0600

    Update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ const NavBar = () => (
       <li><a href="#projects">Projects</a></li>
       <li><a href="#skills">Skills</a></li>
       <li><a href="#music">Music</a></li>
-      <li><a href="#photography">Photography</a></li>
+      <li><a href="#photography">Photo</a></li>
       <li><a href="#contact">Contact</a></li>
     </nav>
   </div>
