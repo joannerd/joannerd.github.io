1a6158c232bff86951918d8602e284f49930392f
Joanna Chen <x.chenj@gmail.com>
2020-05-05 12:49:31 -0700

    Remove dom manipulation

--- src/App.js
+++ src/App.js
@@ -9,32 +9,20 @@ import Photography from './components/Photography';
 import Contact from './components/Contact';
 import NavBar from './components/Navbar';
 
-const App = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-
-    const nav = document.querySelector("nav");
-    const bars = document.querySelector(".bars");
-
-    bars.addEventListener("mouseover", () => nav.classList.remove("hidden"));
-    bars.addEventListener("mouseout", () => nav.classList.add("hidden"));
-  });
-
-  return (
-    <div className="app">
-      <SocialLinks />
-      <main>
-        <Home />
-        <Profile />
-        <ProjectsList />
-        <SkillsList />
-        <Music />
-        <Photography />
-        <Contact />
-      </main>
-      <NavBar />
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <SocialLinks />
+    <main>
+      <Home />
+      <Profile />
+      <ProjectsList />
+      <SkillsList />
+      <Music />
+      <Photography />
+      <Contact />
+    </main>
+    <NavBar />
+  </div>
+);
 
 export default App;
