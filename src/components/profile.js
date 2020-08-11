7d07559da628db640f392553c5d728bb16f77449
Joanna Chen <x.chenj@gmail.com>
2020-08-11 01:37:04 -0700

    Fix formatting

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,7 +5,7 @@ const blurbs = [
   "Hi! My name is Joanna Chen. I'm a musician turned programmer, and I love the art of learning and practicing new things.",
   "While I have a focus in JavaScript, React, and Ruby, I've been learning C# and .NET Core and exploring SuperCollider to start live-coding music.",
   "As a concert producer, my eye for elegant and functional stage design has naturally translated to attention and curiosity in web design.",
-  "I have a keen attention to detail, quick learning skills, and strong values in work ethic that I attribute to my years of experience as a musician.",
+  "I have a keen attention to detail, quick learning skills, and a strong work ethic that I attribute to my years of experience as a musician.",
   "On the weekends, I volunteer as Southern California Marimba's Production/Media Manager.",
 ];
 
