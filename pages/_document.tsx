a4e21a49f668fa7dcbee4cdc3401bdc99630869b
Joanna Chen <joannerdchen@gmail.com>
2021-03-24 20:15:57 -0700

    Add image prefetching

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
+    const images = [
+      '1.png',
+      '2.png',
+      '3.png',
+      '4.png',
+      '5.png',
+      '6.png',
+      '7.png',
+      'addastra.png',
+      'boba-jo.jpg',
+      'boopblocks.gif',
+      'brewer.gif',
+      'owlscrolls.png',
+      'pomoplan.gif',
+      'synewaveshi.gif',
+    ];
+
     return (
       <Html lang="en">
         <Head>
+          {images.map((image) => (
+            <link key={image} rel="prefetch" href={`https://joannerd.github.io/images/${image}`} />
+          ))}
           <script
             src="https://kit.fontawesome.com/51da45b07a.js"
             crossOrigin="anonymous"
