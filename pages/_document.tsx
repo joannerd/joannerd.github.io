89117508c49e4947b2208d6d90c3452ab632e920
Joanna Chen <joannerdchen@gmail.com>
2021-03-25 20:17:47 -0700

    Fix interpolation syntax

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,7 +47,7 @@ class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', ${`${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`});
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
               `,
                 }}
               />
